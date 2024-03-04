f55252c2836930db0963e521a51f5bccf2e4c235
Nia <nia.delhi.2003@gmail.com>
2024-03-04 22:30:57 +0530

    tenth commit

--- src/main/java/com/driver/services/impl/ReservationServiceImpl.java
+++ src/main/java/com/driver/services/impl/ReservationServiceImpl.java
@@ -47,7 +47,8 @@ public class ReservationServiceImpl implements ReservationService {
         List<Spot> filteredSpots = filterSpotsByType(availableSpots, numberOfWheels);
         
         if (filteredSpots.isEmpty()) {
-            throw new NotFoundException("Cannot make reservation");
+//            throw new NotFoundException("Cannot make reservation");
+            return null;
         }
         Spot minPriceSpot = filteredSpots.get(0);
         double minPrice = filteredSpots.get(0).getPricePerHour()*timeInHours;
@@ -60,8 +61,8 @@ public class ReservationServiceImpl implements ReservationService {
         }
 
         if (minPriceSpot == null) {
-//            throw new NotFoundException("Cannot make reservation");
-            return null;
+            throw new NotFoundException("Cannot make reservation");
+
         }
 
         Reservation reservation = new Reservation();
