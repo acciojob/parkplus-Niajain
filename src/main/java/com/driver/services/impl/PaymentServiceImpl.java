dbdc9537aa72230daffdb9671425552af109d2a2
Nia <nia.delhi.2003@gmail.com>
2024-03-05 04:50:54 +0530

    30th commit

--- src/main/java/com/driver/services/impl/PaymentServiceImpl.java
+++ src/main/java/com/driver/services/impl/PaymentServiceImpl.java
@@ -21,86 +21,52 @@ public class PaymentServiceImpl implements PaymentService {
     PaymentRepository paymentRepository2;
 
     @Override
-    public Payment pay(Integer reservationId, int amountSent, String mode) throws Exception {
-        PaymentMode paymentMode;
-        try {
-            paymentMode = PaymentMode.valueOf(mode.toUpperCase());
-        }
-        catch(RuntimeException e){
-            throw new RuntimeException("Payment mode not detected");
-        }
 
+    public Payment pay(Integer reservationId, int amountSent, String mode) throws Exception {
         Reservation reservation = reservationRepository2.findById(reservationId).get();
+
         if(reservation.getPayment()!=null){
             Payment payment = reservation.getPayment();
             payment.setPaymentCompleted(true);
             return payment;
         }
+
+        double billAmount=calculateBillAmount(reservation);
+
+        if(amountSent<billAmount)
+            throw new NotFoundException("Insufficient Amount");
+
         Spot spot = reservation.getSpot();
-        if(reservation.getNumberOfHours()*spot.getPricePerHour() > amountSent){
-            throw new RuntimeException("Insufficient Amount");
-        }
 
-        Payment payment = new Payment();
-        payment.setPaymentMode(paymentMode);
-        payment.setPaymentCompleted(true);
+        PaymentMode paymentMode=validatePaymentMode(mode);
+
+        Payment payment=new Payment();
         payment.setReservation(reservation);
+        payment.setPaymentCompleted(true);
+        payment.setPaymentMode(paymentMode);
 
-        spot.setOccupied(false); //update
+        spot.setOccupied(false);
 
         reservation.setPayment(payment);
-
         reservationRepository2.save(reservation);
+
         return payment;
     }
+
+    private PaymentMode validatePaymentMode(String mode) {
+        String upperCaseMode = mode.toUpperCase();
+        if(upperCaseMode.equals("CASH")|| upperCaseMode.equals("CARD") || upperCaseMode.equals("UPI"))
+        {
+            return PaymentMode.valueOf(upperCaseMode);
+        }
+        else
+        {
+            throw new NotFoundException("Payment mode not detected");
+        }
+    }
+
+    private double calculateBillAmount(Reservation reservation) {
+
+        return reservation.getSpot().getPricePerHour()*reservation.getNumberOfHours();
+    }
 }
-
-
-//    public Payment pay(Integer reservationId, int amountSent, String mode) throws Exception {
-//        Reservation reservation = reservationRepository2.findById(reservationId).get();
-//
-//        if(reservation.getPayment()!=null){
-//            Payment payment = reservation.getPayment();
-//            payment.setPaymentCompleted(true);
-//            return payment;
-//        }
-//
-//        double billAmount=calculateBillAmount(reservation);
-//
-//        if(amountSent<billAmount)
-//            throw new NotFoundException("Insufficient Amount");
-//
-//        Spot spot = reservation.getSpot();
-//
-//        PaymentMode paymentMode=validatePaymentMode(mode);
-//
-//        Payment payment=new Payment();
-//        payment.setReservation(reservation);
-//        payment.setPaymentCompleted(true);
-//        payment.setPaymentMode(paymentMode);
-//
-//        spot.setOccupied(false);
-//
-//        reservation.setPayment(payment);
-//        reservationRepository2.save(reservation);
-//
-//        return payment;
-//    }
-//
-//    private PaymentMode validatePaymentMode(String mode) {
-//        String upperCaseMode = mode.toUpperCase();
-//        if(upperCaseMode.equals("CASH")|| upperCaseMode.equals("CARD") || upperCaseMode.equals("UPI"))
-//        {
-//            return PaymentMode.valueOf(upperCaseMode);
-//        }
-//        else
-//        {
-//            throw new NotFoundException("Payment mode not detected");
-//        }
-//    }
-//
-//    private double calculateBillAmount(Reservation reservation) {
-//
-//        return reservation.getSpot().getPricePerHour()*reservation.getNumberOfHours();
-//    }
-//}
